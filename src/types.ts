6631c95822a0fc61b987ed19ba5d6223ff283014
weepwood <weepwood@outlook.com>
2026-07-25 14:57:43 +0800

    重构：提取插件配置与语雀数据类型

---
+++ src/types.ts
@@ -0,0 +1,35 @@
+export interface YuqueSyncSettings {
+	yuqueToken: string;
+	defaultBookId: string;
+	yuqueCookie: string;
+}
+
+export const DEFAULT_SETTINGS: YuqueSyncSettings = {
+	yuqueToken: '',
+	defaultBookId: '',
+	yuqueCookie: '',
+};
+
+export interface YuqueLocation {
+	bookId: string;
+	slug: string;
+}
+
+export interface YuqueDocument {
+	title: string;
+	content: string;
+	updatedAt: string;
+}
+
+export interface CreatedYuqueDocument {
+	id: number;
+	slug: string;
+}
+
+export interface ImageReference {
+	path: string;
+	pathStart: number;
+	pathEnd: number;
+	fullStart: number;
+	fullEnd: number;
+}
